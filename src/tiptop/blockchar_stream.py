0ba31db6f2ce4cfcd7d4e44dc8260df2f5efd1e4
Nico Schlömer <nico.schloemer@gmail.com>
2021-09-24 20:26:37 +0200

    more stream tests

--- src/tiptop/blockchar_stream.py
+++ src/tiptop/blockchar_stream.py
@@ -4,13 +4,25 @@ num_to_blockchar = [" ", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"]
 
 
 class BlockCharStream:
-    def __init__(self, num_chars: int, minval: float, maxval: float):
-        self.graph = " " * num_chars
+    def __init__(self, width: int, height: int, minval: float, maxval: float):
+        self.graph = [" " * width] * height
+        self.height = height
         self.minval = minval
         self.maxval = maxval
         self.last_value: float = minval
 
     def add_value(self, value):
-        k = ceil((value - self.minval) / (self.maxval - self.minval) * 8)
-        self.graph = self.graph[1:] + num_to_blockchar[k]
+        k = ceil((value - self.minval) / (self.maxval - self.minval) * 8 * self.height)
+
+        blocks = [8] * (k // 8)
+        if k % 8 > 0:
+            blocks += [k % 8]
+        blocks += [0] * (self.height - len(blocks))
+
+        chars = [num_to_blockchar[i] for i in blocks]
+
+        # update stream
+        for k, char in enumerate(chars[::-1]):
+            self.graph[k] = self.graph[k][1:] + char
+
         self.last_value = value
--- tests/test_streams.py
+++ tests/test_streams.py
@@ -24,26 +24,51 @@ def test_braille_stream_tall():
     assert stream.graph == ["   ", "   ", "  ⢰", "  ⢸"]
 
     stream.add_value(10.0)
-    assert stream.graph == ['   ', '   ', '  ⡆', '  ⣧']
+    assert stream.graph == ["   ", "   ", "  ⡆", "  ⣧"]
 
-    stream.add_value(99.0)
-    assert stream.graph == ['  ⢸', '  ⢸', ' ⢰⢸', ' ⢸⣼']
+    stream.add_value(100.0)
+    assert stream.graph == ["  ⢸", "  ⢸", " ⢰⢸", " ⢸⣼"]
 
     stream.add_value(76.0)
-    assert stream.graph ==  ['  ⣇', '  ⣿', ' ⡆⣿', ' ⣧⣿']
+    assert stream.graph == ["  ⣇", "  ⣿", " ⡆⣿", " ⣧⣿"]
+
+    stream.add_value(0.0)
+    assert stream.graph == [" ⢸⡀", " ⢸⡇", "⢰⢸⡇", "⢸⣼⡇"]
 
 
 def test_blockchar_stream():
-    stream = tiptop.BlockCharStream(5, 0.0, 100.0)
+    stream = tiptop.BlockCharStream(5, 1, 0.0, 100.0)
 
     stream.add_value(10.0)
-    assert stream.graph == "    ▁"
+    assert stream.graph == ["    ▁"]
 
     stream.add_value(30.0)
-    assert stream.graph == "   ▁▃"
+    assert stream.graph == ["   ▁▃"]
 
     stream.add_value(60.0)
-    assert stream.graph == "  ▁▃▅"
+    assert stream.graph == ["  ▁▃▅"]
 
-    stream.add_value(90.0)
-    assert stream.graph == " ▁▃▅█"
+    stream.add_value(100.0)
+    assert stream.graph == [" ▁▃▅█"]
+
+    stream.add_value(0.0)
+    assert stream.graph == ["▁▃▅█ "]
+
+
+def test_blockchar_stream_tall():
+    stream = tiptop.BlockCharStream(3, 4, 0.0, 100.0)
+
+    stream.add_value(10.0)
+    assert stream.graph == ["   ", "   ", "   ", "  ▄"]
+
+    stream.add_value(30.0)
+    assert stream.graph == ["   ", "   ", "  ▂", " ▄█"]
+
+    stream.add_value(60.0)
+    assert stream.graph == ["   ", "  ▄", " ▂█", "▄██"]
+
+    stream.add_value(100.0)
+    assert stream.graph == ["  █", " ▄█", "▂██", "███"]
+
+    stream.add_value(0.0)
+    assert stream.graph == [" █ ", "▄█ ", "██ ", "██ "]
