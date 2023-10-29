4a2596a69a4f9756aac437a091ee0833e6b6e7cf
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-10-30 01:37:16 +0530

    fix: cleaning comments

--- src/tiptop/_app.py
+++ src/tiptop/_app.py
@@ -100,13 +100,11 @@ def run(argv=None):
     try:
         TiptopApp.run(log=args.log)
     except TypeError as e:
-        print(args)
         if "run() got an unexpected keyword argument 'log'" in e.args[0]:
             try:
                 from textual import __version__ as textual_version
             except:
                 textual_version = "0"
-            # print in red "You're using an incorrect version of textual." Please check "https://github.com/nschloe/tiptop/issues/109"
             debug_info = (
                 "\033[91m"
                 "You're using an incorrect version of textual.\n"
